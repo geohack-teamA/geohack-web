31cb7ec4ac45005c45a7daa0fb8ea836c6c39760
HideBa <baba.papa1120.ba@gmail.com>
2022-02-13 07:01:37 +0900

    feat: update liff login

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 import styles from './Home.module.css'
 import useLine from "@geohack/repository/liff"
 
-const Home: NextPage = () => {
-  const {getLanguage} = useLine();
+const Home = () => {
+  const {getLanguage, isLoggedIn, logout, login} = useLine();
 return (
     <div className={styles.container}>
       <Head>
@@ -16,50 +16,10 @@ return (
       </Head>
 
       <main className={styles.main}>
-
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <div>
-          Your language is
-        </div>
-        <div>
-          {getLanguage()}
-        </div>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        You
+        <div>{isLoggedIn()}</div>
+        <button type='button' onClick={() => login()}>Login</button>
+        <button type='button' onClick={() => logout()}>Logout</button>
       </main>
 
       <footer className={styles.footer}>
--- src/repository/liff.ts
+++ src/repository/liff.ts
@@ -8,11 +8,13 @@ export default () => {
     return () => console.log("hello")
   }
   useEffect(() => {
+    const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
+    if(!liffId)return;
     if(typeof window !== "undefined"){
     const initLiff =async () => {
         try{
           liff.init({
-          liffId: process.env.NEXT_PUBLIC_LIFF_ID ?? "",
+          liffId,
           // withLoginOnExternalBrowser: true
         })
       }catch(err){
@@ -23,7 +25,12 @@ export default () => {
   }
   }, [])
   const isLoggedIn = () => {
-    return liff.isLoggedIn();
+    if(!liff.ready)return;
+    return liff.isLoggedIn(); //TODO: this causes error
+  }
+  const login = () => {
+  if(isLoggedIn())return;
+  return liff.login();
   }
   const logout = () => {
     if(!isLoggedIn()) return;
@@ -31,6 +38,9 @@ export default () => {
     window.location.reload()
   }
   return {
-    getLanguage
+    getLanguage,
+    isLoggedIn,
+    login,
+    logout
   }
 }
